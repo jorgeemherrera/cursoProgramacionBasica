d65b7150822990c369b41febbe324f9024ff7ac3
Jorge Eliecer <jorge.m.h@outlook.com>
2019-12-15 22:07:46 -0500

    funciones en JavaScript

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,9 +2,16 @@ dibujo = document.getElementById('dibujo');
 lienzo = dibujo.getContext('2d');
 console.log(lienzo);
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+// Function
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xInicial, yInicial);
+  lienzo.lineTo(xFinal, yFinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+dibujarLinea('pink', 10, 300, 220, 10);
+dibujarLinea('blue', 310, 10, 10, 220);
